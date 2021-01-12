28ee7bf9e339539448206e9a8c38f6658f9d8792
ccev <42342921+ccev@users.noreply.github.com>
2021-01-13 00:17:54 +0100

    include pogoinfo

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-version = "0.0.1"
+version = "0.0.2"
 
 setup(
     name="pogodata",
@@ -8,7 +8,7 @@ setup(
     url="https://github.com/ccev/pogodata",
     version=version,
     install_requires=["requests"],
-    packages=["pogodata"],
+    packages=["pogodata", "pogodata.pogoinfo"],
     long_description="For documentation, plase visit https://github.com/ccev/pogodata",
     description="Easy and up-to-date Pogo Data"
 )
